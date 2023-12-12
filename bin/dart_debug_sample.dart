6e5ebfcac267c510f80288004685cf6d97c77e48
Hyojun Cho <lionjerher84@gmail.com>
2023-12-12 21:45:24 +0900

    디버깅 오류 수정

--- bin/dart_debug_sample.dart
+++ bin/dart_debug_sample.dart
@@ -1,8 +1,17 @@
-import 'package:dart_debug_sample/dart_debug_sample.dart' as dart_debug_sample;
 import 'package:dart_debug_sample/dart_debug_sample.dart';
+
 
 void main() {
   YukymController yukymController = YukymController();
-  print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
-  print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
+
+
+  // 해당 월에 맞는 자시의 국 표시
+  String a = yukymController.getTyA();
+  print(a);
+
+  // 해당 시간에 맞는 자시의 국 표시
+  String b = yukymController.getTyB();
+  print(b);
+
+
 }
